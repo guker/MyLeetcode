7b3c6b9863906dc7275aba927b331be8bd74018c
hqwsky <huqunwei2607@gmail.com>
2015-08-13 15:47:22 +0800

    Update UniquePath.cpp

--- UniquePath.cpp
+++ UniquePath.cpp
@@ -58,6 +58,19 @@ int uniquePaths(int m, int n)
     return u;
 }
 
+int uniquePaths1(int m, int n) 
+{  
+	vector<vector<int>> v(m, vector<int>(n, 1));  
+	for(int i=1; i<m; ++i)
+	{  
+		for(int j=1; j<n; ++j)
+		{  
+		    v[i][j]=v[i-1][j]+v[i][j-1];  
+		}  
+	}  
+	return v[m-1][n-1];  
+} 
+
 void printMatrix(int*a, int m, int n)
 {
     for (int i=0; i<m; i++){
