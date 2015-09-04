9fc7ba10ad334f5125933540c0bb7792cf7a4015
guker <huqunwei2607@gmail.com>
2015-09-04 16:24:27 +0800

    linked list cycle

---
+++ linkedListCycle.cpp
@@ -0,0 +1,37 @@
+﻿/********************************************************************************** 
+* 
+* Given a linked list, determine if it has a cycle in it.
+* 
+* Follow up:
+* Can you solve it without using extra space?
+* 
+*               
+**********************************************************************************/
+
+/*
+* if there is a cycle in the list, then we can use two pointers travers the list.
+*
+* one pointer traverse one step each time, another one traverse two steps each time.
+*
+* so, those two pointers meet together, that means there must be a cycle inside the list.
+*/
+#include<iostream>
+struct ListNode 
+{
+	int val;
+	ListNode *next;
+	ListNode(int x) : val(x), next(NULL) {}
+};
+bool hasCycle(ListNode *head)
+{
+	if (head==NULL || head->next==NULL) return false;
+	ListNode* fast=head;
+	ListNode* slow=head;
+	do
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+	}while(fast != NULL && fast->next != NULL && fast != slow);
+
+	return fast == slow? true : false;
+} 
